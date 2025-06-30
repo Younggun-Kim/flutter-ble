31f1e9c20a46b677fb1c3577066284977e34af5d
younggun.kim <younggun.kiim@ikoob.com>
2025-06-30 10:24:05 +0900

    feat: BLoC 로깅 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ble/core/di/di.dart';
 import 'package:flutter_ble/presentation/presentation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 
 import 'core/core.dart';
 
 void main() {
+  Bloc.observer = TalkerBlocObserver(
+    settings: TalkerBlocLoggerSettings(),
+  );
+
   BluetoothManager.init();
 
   configureDependencies();
